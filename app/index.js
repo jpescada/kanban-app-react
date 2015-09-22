5cb8eb43e5d742d16f93a174331694c8a0d90e34
Joao Pescada <joao@pescada.pt>
2015-09-22 12:11:14 +0100

    Added data persistence with localStorage

--- app/index.js
+++ app/index.js
@@ -1,12 +1,17 @@
 require('./main.css');
 
-import 'array.prototype.findindex';
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
 import React from 'react';
 import App from './components/App.jsx';
 
 main();
 
 function main(){
+
+	persist( alt, storage, 'app');
+	
 	const app =  document.createElement('div');
 
 	document.body.appendChild(app);
